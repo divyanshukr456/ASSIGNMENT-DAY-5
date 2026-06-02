ba72641171f144afeb4af297798fb2f9f6666624
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-06-02 12:07:30 +0530

    DAY-5

---
+++ q18.py
@@ -0,0 +1,18 @@
+n = int(input("Enter a number: "))
+temp = n
+sum_fact = 0
+
+while temp > 0:
+    digit = temp % 10
+    fact = 1
+
+    for i in range(1, digit + 1):
+        fact *= i
+
+    sum_fact += fact
+    temp //= 10
+
+if sum_fact == n:
+    print("Strong Number")
+else:
+    print("Not a Strong Number")
